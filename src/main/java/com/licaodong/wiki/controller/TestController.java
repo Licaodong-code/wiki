d454080ef05fb8b653c86c6114b9e9c0360e130c
Licaodong-code <1776782142@qq.com>
2022-03-22 21:14:07 +0800

    一个简单的test例子

--- src/main/java/com/licaodong/wiki/controller/TestController.java
+++ src/main/java/com/licaodong/wiki/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.licaodong.wiki.controller;
 
+import com.licaodong.wiki.domain.Test;
+import com.licaodong.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 //@Controller
@@ -16,6 +21,8 @@ public class TestController {
 //    @RequestMapping("/hello")
     @Value("${test.hello:TEST}")
     private String testHello;
+    @Resource
+    private TestService testService;
 
     @GetMapping("/hello")
     public String hello(){
@@ -25,5 +32,10 @@ public class TestController {
     @PostMapping("/helloPost")
     public String helloPost(String name){
         return "HELLO　WORLD POST!" + name;
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list(){
+        return testService.list();
     }
 }
