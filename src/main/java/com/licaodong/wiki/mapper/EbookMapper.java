356db25a6f76b16f3684694993cc738d25b695fa
Licaodong-code <1776782142@qq.com>
2022-03-22 23:01:51 +0800

    完成电子书查询接口

--- src/main/java/com/licaodong/wiki/mapper/EbookMapper.java
+++ src/main/java/com/licaodong/wiki/mapper/EbookMapper.java
@@ -2,8 +2,9 @@ package com.licaodong.wiki.mapper;
 
 import com.licaodong.wiki.domain.Ebook;
 import com.licaodong.wiki.domain.EbookExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface EbookMapper {
     long countByExample(EbookExample example);
