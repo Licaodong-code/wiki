fd4000f2b0d5e29dfd551f24c5e01928e3301fc7
Licaodong-code <1776782142@qq.com>
2022-03-22 23:32:44 +0800

    增加通用返回类Common resp

---
+++ src/main/java/com/licaodong/wiki/controller/EbookController.java
@@ -0,0 +1,27 @@
+package com.licaodong.wiki.controller;
+
+import com.licaodong.wiki.domain.Ebook;
+import com.licaodong.wiki.resp.CommonResp;
+import com.licaodong.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
+    }
+}
