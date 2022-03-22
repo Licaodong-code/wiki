1d3515ad566cb4df43b9b28c78926c960b934dba
Licaodong-code <1776782142@qq.com>
2022-03-22 16:42:10 +0800

    集成热部署

--- src/main/java/com/licaodong/wiki/controller/TestController.java
+++ src/main/java/com/licaodong/wiki/controller/TestController.java
@@ -1,12 +1,9 @@
 package com.licaodong.wiki.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 //@Controller
@@ -17,9 +14,12 @@ public class TestController {
 
      */
 //    @RequestMapping("/hello")
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello(){
-        return "HELLO　WORLD!";
+        return "HELLO　WORLD!"+testHello;
     }
 
     @PostMapping("/helloPost")
