c84bccce7e23350c1160da1d7cfde1c9ee77f2a6
Licaodong-code <1776782142@qq.com>
2022-03-24 23:59:21 +0800

    多环境配置 baseurl设置

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -120,7 +120,7 @@ export default defineComponent({
     ];
     onMounted(()=>{
       console.log("onMounted");
-      axios.get("http://localhost:8088/ebook/list").then((response) => {
+      axios.get("/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         // ebooks1.books = data.content;
