5e6c5c89ff7343015a39b68f04b030a3e1d19cc2
Licaodong-code <1776782142@qq.com>
2022-03-27 12:54:07 +0800

    集成validation,为列表接口增加参数校验

--- src/main/java/com/licaodong/wiki/controller/EbookController.java
+++ src/main/java/com/licaodong/wiki/controller/EbookController.java
@@ -9,6 +9,7 @@ import com.licaodong.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/ebook")
@@ -18,7 +19,7 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(EbookQueryReq ebookQueryReq) {
+    public CommonResp list(@Valid EbookQueryReq ebookQueryReq) {
         CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp<>();
         PageResp<EbookQueryResp> pageResp = ebookService.list(ebookQueryReq);
         resp.setContent(pageResp);
